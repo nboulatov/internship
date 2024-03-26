ff249482989b64aebd6d7466bb138fab0dd7943f
nboulatov <nboulatov@gmail.com>
2024-03-26 08:58:21 -0700

    updated files for Allure report

--- features/environment.py
+++ features/environment.py
@@ -7,51 +7,31 @@ from selenium.webdriver.support.wait import WebDriverWait
 from app.application import Application
 
 
-# def browser_init(context, browser_name, headless=False):
-#     if browser_name == 'chrome':
-#         chrome_options = webdriver.ChromeOptions()
-#         if headless:
-#             chrome_options.add_argument('--headless')
-#             chrome_options.add_argument("--window-size=1920x1080")
-#         context.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
-#         if not headless:
-#             context.driver.maximize_window()
-#     elif browser_name == 'firefox':
-#         firefox_options = webdriver.FirefoxOptions()
-#         if headless:
-#             firefox_options.add_argument('--headless')
-#             firefox_options.add_argument("--window-size=1920x1080")
-#         context.driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()), options=firefox_options)
-#         if not headless:
-#             context.driver.maximize_window()
-#     else:
-#         raise ValueError(f"Unsupported browser: {browser_name}.\nPlease use 'chrome' or 'firefox'.")
-#
-#     context.wait = WebDriverWait(context.driver, 10)
-#     context.app = Application(context.driver)
-#
+def browser_init(context, browser_name, headless=False):
+    if browser_name == 'chrome':
+        chrome_options = webdriver.ChromeOptions()
+        if headless:
+            chrome_options.add_argument('--headless')
+            chrome_options.add_argument("--window-size=1920x1080")
+        context.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+        if not headless:
+            context.driver.maximize_window()
+    elif browser_name == 'firefox':
+        firefox_options = webdriver.FirefoxOptions()
+        if headless:
+            firefox_options.add_argument('--headless')
+            firefox_options.add_argument("--window-size=1920x1080")
+        context.driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()), options=firefox_options)
+        if not headless:
+            context.driver.maximize_window()
+    else:
+        raise ValueError(f"Unsupported browser: {browser_name}.\nPlease use 'chrome' or 'firefox'.")
 
-def browser_init(context):
-    user_name = ''
-    access_key = ''
-    browser_stack_url = f'http://{user_name}:{access_key}@hub-cloud.browserstack.com/wd/hub'
-    options = Options()
-    browser_stack_options = {
-        'os': 'OS X',
-        'osVersion': 'Ventura',
-        'browserName': 'Chrome',
-        'sessionName': 'Target Search'
-    }
-    options.set_capability('bstack:options', browser_stack_options)
-    context.driver = webdriver.Remote(command_executor=browser_stack_url, options=options)
-
-    context.driver.maximize_window()
     context.wait = WebDriverWait(context.driver, 10)
     context.app = Application(context.driver)
 
 def before_scenario(context, scenario):
     print('\nStarted scenario: ', scenario.name)
-    browser_init(context)
 
 def before_step(context, step):
     print('\nCurrent step: ', step.name)
--- features/tests/tests.feature
+++ features/tests/tests.feature
@@ -2,15 +2,15 @@ Feature: Test Scenarios for Reelly
 
   @smoke
   Scenario: User is able to login to Reelly
-#    Given I use browser options: chrome headless
+    Given I use browser options: chrome headless
     Given I navigate to: https://soft.reelly.io/sign-in
     When I input username: nboulatov@gmail.com
     When I input password: 123
     When I click button: Continue
     Then I verify page is displayed: Main Page
-
+  @smoke
   Scenario Outline: User is able to see social media icons
-#    Given I use browser options: <browser> <headless_or_not>
+    Given I use browser options: <browser> <headless_or_not>
     Given I navigate to: https://soft.reelly.io/sign-in
     When I input username: nboulatov@gmail.com
     When I input password: 123
